86eadc3251e40758be48a57cc01b37839f19fdc4
kylianjoff <kylianju382@gmail.com>
2025-12-04 18:28:29 +0100

    Ajout de la validation Jwt

--- GameServerApi/Program.cs
+++ GameServerApi/Program.cs
@@ -2,6 +2,11 @@ using Microsoft.EntityFrameworkCore.Sqlite;
 using GameServerApi.Models;
 using Scalar.AspNetCore;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+
 namespace GameServerApi;
 
 public class Program
@@ -20,6 +25,24 @@ public class Program
         {
             options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
         });
+
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ClockSkew = TimeSpan.FromMinutes(10), // Temps de tolérance pour la date d'expiration
+                    ValidateLifetime = true, // Vérifie la date d'expiration
+                    ValidateIssuerSigningKey = true, // Vérifie la signature
+                    ValidAudience = "localhost:5000", // Qui peut utiliser le token ici c'est notre API
+                    ValidIssuer = "localhost:5000", // Qui émet le token ici c'est notre API
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("TheSecretKeyThatShouldBeStoredInTheConfiguration")
+                    ),
+                    RoleClaimType = ClaimTypes.Role // Dans quel claim est stocké le role
+                };
+            });
 
         var app = builder.Build();
 
